9bd3e62cb4a63b4d8d7ba9c83c05f7c2f455b62d
cnpink35 <33958963+cnpink35@users.noreply.github.com>
2023-04-26 16:01:04 +0900

    Update delRow.js

--- delRow.js
+++ delRow.js
@@ -5,3 +5,14 @@ function delRow(){
   }
 }
 
+
+
+function divCol(){
+  var row = document.getElementById('table');
+  if(row.rows.length>0){
+  var cell0 = row.insertCell(0);
+  var cell1 = row.insertCell(1);
+    cell0.innerText = test0;
+    cell1.innerText = test0;
+  }
+}
