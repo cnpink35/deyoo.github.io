531f0bbbd960f9966d6f5242374f867a55e4c309
cnpink35 <33958963+cnpink35@users.noreply.github.com>
2023-04-26 16:26:50 +0900

    Update addRow.js

--- addRow.js
+++ addRow.js
@@ -1,8 +1,4 @@
+
 function addRow(){
-  //$("#table").after("<tr><td> test </td></tr>");
-  
-  var row = document.getElementById('table');
-  row.insertRow();
-  var cell = row.inserCell(0);
-  cell.innerText = 'test';
+  $("#table").append("<tr><td>  </td></tr>");
 }
