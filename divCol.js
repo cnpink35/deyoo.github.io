9f7ca4064d81970fecf31d1b10958250bff70d3b
cnpink35 <33958963+cnpink35@users.noreply.github.com>
2023-04-27 10:08:15 +0900

    Update divCol.js

--- divCol.js
+++ divCol.js
@@ -1,11 +1,14 @@
 
 function divCol(){
   var row = document.getElementById('table');
-  if(row.rows.length>0){
-  var cell0 = row.insertCell(0);
+  var length = row.rows.length;
+  if(length>0){
+  /*var cell0 = row.insertCell(0);
   var cell1 = row.insertCell(1);
     cell0.innerText = test0;
-    cell1.innerText = test0;
+    cell1.innerText = test0;*/
+    var cell = row.rows[length-1].insertCell(-1);
+    cell.innerText = 'test';
   }
 }
     
