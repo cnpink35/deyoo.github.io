b45a623838230c2578c829c78e7c1d410ea3cce2
cnpink35 <33958963+cnpink35@users.noreply.github.com>
2023-04-27 10:21:18 +0900

    Update divCol.js

--- divCol.js
+++ divCol.js
@@ -1,24 +1,14 @@
+/*
+divide last row of the table
+made by daeun yoo on 2023.04.27
+*/
 
 function divCol(){
   var row = document.getElementById('table');
   var length = row.rows.length;
   if(length>0){
-  /*var cell0 = row.insertCell(0);
-  var cell1 = row.insertCell(1);
-    cell0.innerText = test0;
-    cell1.innerText = test0;*/
     var cell = row.rows[length-1].insertCell(-1);
-    cell.innerText = 'test';
+    /*cell.innerText = 'test';*/
   }
 }
-    
 
-/*function addRow(){
-  //$("#table").after("<tr><td> test </td></tr>");
-  
-  var row = document.getElementById('table');
-  var newRow.insertRow();
-  var cell = newRow.inserCell(0);
-  cell.innerText = 'test';
-}
-*/
